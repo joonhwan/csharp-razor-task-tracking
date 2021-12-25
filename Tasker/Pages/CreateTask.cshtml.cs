3aafa58fb9b8cee902157558a344182911489c56
joonhwan <joonhwan.lee@gmail.com>
2021-12-26 00:02:41 +0900

    fixed

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -1,12 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Tasker.Pages
 {
     public class CreateTask : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
+        
         public void OnGet()
         {
             
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
